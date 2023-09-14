55d099af76d717933fdf26ac26547a35117237d5
Ryan McDonagh <laslomcd@gmail.com>
2023-09-14 10:12:26 -0400

    finished project

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:first_app/gradient_container.dart';
+
+const colors = [
+  Color.fromARGB(255, 26, 2, 80),
+  Color.fromARGB(255, 45, 7, 98),
+];
+
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 26, 2, 80),
-              Color.fromARGB(255, 45, 7, 98),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white
-                ),
-            ),
-          ),
-        ),
+        body: GradientContainer(colors),
       ),
     ),
   );
 }
-
